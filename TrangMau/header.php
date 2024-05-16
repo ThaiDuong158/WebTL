199145e34ced02d73492c5b2a1cb0bab29332742
ThaiDuong158 <phandangthaiduong1508@gmail.com>
2024-05-16 23:52:59 +0700

    commit

---
+++ TrangMau/header.php
@@ -0,0 +1,37 @@
+<header class="header">
+    <div class="row">
+        <div class="sidebar-width col-2 bg-success d-flex">
+            <a href="../TrangMau/index.php" class="container-fluid d-inline-flex justify-content-center align-items-center">
+                <img src="../icon/iconVLUTE.png" class="header__icon" alt="">
+                <p class="text-white sidebar-hide">VLUTE</p>
+            </a>
+        </div>
+        <div class="col container-fluid d-inline-flex justify-content-between">
+            <i class="fa-solid fa-bars sidebar-mini text-white align-content-center header__bar"></i>
+            <div class="d-inline-flex no-select align-items-center text-white">
+                <div class="header__login d-none align-items-center text-white">
+                    <i class="fa-solid fa-right-to-bracket"></i>
+                    <p>Đăng nhập</p>
+                </div>
+                <div class="header__login d-inline-flex  align-items-center text-white">
+                    <img src="../icon/basicUser.jpg" class="rounded-circle" alt="Cinque Terre" width="25" height="25">
+                    <p>Phan Đặng Thái Dương</p>
+                    <div class="dropdown-list" style="width: 280px;">
+                        <div class="dropdown-info d-flex flex-column align-items-center">
+                            <img src="../icon/basicUser.jpg" class="rounded-circle" alt="" width="90" height="90">
+                            <div class="dropdown-text">
+                                <p>Phan Đặng Thái Dương</p>
+                                <p>21004277</p>
+                                <p>Sinh Viên - 1CTT21A3</p>
+                            </div>
+                        </div>
+                        <div class="dropdown-setting d-flex justify-content-between">
+                            <button class="dropdown-mk">Đổi mật khẩu</button>
+                            <button class="dropdown-dx">Đăng xuất</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ TrangMau/sidebar.php
@@ -0,0 +1,10 @@
+<div class="sidebar sidebar-width col-sm-2">
+    <a href="../TrangMau/index.php" class="sidebar-item left-line no-select">
+        <i class="sidebar-icon fa-solid fa-house"></i>
+        <p class="sidebar-hide">Trang chủ</p>
+    </a>
+    <a href="../DiemDanh/DiemDanh.php" class="sidebar-item no-select">
+        <i class="sidebar fa-solid fa-calendar-days"></i>
+        <p class="sidebar-hide">Điểm danh</p>
+    </a>
+</div>
